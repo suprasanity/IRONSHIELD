6502eb98185e05f62d5868c1ceebd5750291fc5d
yann <jeanmaireyann.pro@gmail.com>
2023-04-04 23:15:46 +0200

    update cron job  to send it every week

--- src/main/java/com/example/ironshield/service/Finder.java
+++ src/main/java/com/example/ironshield/service/Finder.java
@@ -3,7 +3,6 @@ package com.example.ironshield.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -22,44 +21,29 @@ public class Finder {
     public static final String PATTERN = "sshd.*session opened for user.*";
 
     public String fileName="auth.log";
-
-    @Scheduled(fixedRate = 1000000)
+    @Scheduled(fixedRate = 10000)
     public void readFile() {
-        Boolean finded = false;
-StringBuilder sb = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(
-                Runtime.getRuntime().exec(String.format("grep -a %s %s", PATTERN, fileName))
-                        .getInputStream()))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                logger.info(line);
-                sb.append(line);
-                sb.append('\n');
-                finded=true;
-
-            }
-            addLineToFile(sb);
-            if(finded){
-                mail.send("yann.jeanmaire@gmail.com","rapport log critique ","Bonjour yann ",new File("log.txt"));}
-
-        } catch (IOException e) {
-                logger.error(e.getMessage());
-        }
-
-    }
-    public void addLineToFile(StringBuilder line) throws IOException {
-
-            File file = new File("log.txt");
-            try (FileWriter writer = new FileWriter(file,true)) {
-                if (!file.exists()) { // if file does not exist, create it
-                    file.createNewFile();
+        boolean found = false;
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("grep", "-a", PATTERN, fileName);
+            Process process = processBuilder.start();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                 PrintWriter writer = new PrintWriter(new FileWriter("output.txt"))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    logger.info(line);
+                    writer.println(line);
+                    found = true;
                 }
-                writer.write(line.toString());
-
-            } catch (IOException e) {
-              logger.warn(e.getMessage());
             }
+            if (found) {
+                mail.send("yann.jeanmaire@gmail.com", "rapport log critique", "Bonjour yann",
+                        new File("output.txt"));
+            }
+        } catch (IOException e) {
+            logger.error(e.getMessage());
         }
     }
+}
 
 
